4fd8d9135ad1c5f757f50f8d871c0e47bde73719
Srinadh1998-hub <142966311+Srinadh1998-hub@users.noreply.github.com>
2023-10-25 17:48:59 -0500

    completed the bonus part

---
+++ routes/computation.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+    fake_url = "https://fake.com/path" + req.url
+    const url = new URL(fake_url)
+    const search_params = url.searchParams
+    if (req.method === 'GET') {
+        val = search_params.get("x")
+        if(val === null)
+        {
+            val=Math.round(Math.random()*512);
+        }     
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('Math.abs() applied to ' + val + ' is ' + Math.abs(val));
+        res.write('<br>Math.acos() applied to ' + val + ' is ' + Math.acos(val))
+        res.write('<br>Math.sin() applied to ' + val + ' is ' + Math.sin(val))
+        res.end()
+    }
+
+});
+
+module.exports = router;
